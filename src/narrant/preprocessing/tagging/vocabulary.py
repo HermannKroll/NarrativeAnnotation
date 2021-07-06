38b05c1c7bbefa5b62ec144b7c0e1f7dfedb886b
jan <j.pirklbauer@tu-bs.de>
2021-07-06 07:56:24 +0200

    bugfixes + added tests for vocabdictpreprocess

--- src/narrant/preprocessing/tagging/vocabulary.py
+++ src/narrant/preprocessing/tagging/vocabulary.py
@@ -18,7 +18,11 @@ class Vocabulary:
         with open(self.path, "r") as f:
             reader = csv.DictReader(f, delimiter="\t")
             for line in reader:
-                for syn in {s for t in (line["synonyms"].split(";") + [line["heading"]]) for s in expand_vocabulary_term(t.lower()) if t}:
+                if not line["heading"] or not line["enttype"] or not line["id"]:
+                    continue
+                for syn in {s
+                            for t in (line["synonyms"].split(";") if line["synonyms"] else [] )+ [line["heading"]]
+                            for s in expand_vocabulary_term(t.lower()) if t}:
                     self.vocabularies[line["enttype"]][syn] |= {line["id"]}
             self.vocabularies = {k: dict(v) for k, v in self.vocabularies.items()}
 
