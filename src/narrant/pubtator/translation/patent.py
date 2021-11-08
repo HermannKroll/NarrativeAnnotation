f311434f25f5da35ba74eb5e36fd3830978f27d7
HermannKroll <hermann.kroll@gmail.com>
2021-11-08 12:46:39 +0100

    update patent convert

--- src/narrant/pubtator/translation/patent.py
+++ src/narrant/pubtator/translation/patent.py
@@ -1,10 +1,10 @@
 import logging
-import os
 import re
 import sys
 from argparse import ArgumentParser
 
 from narrant.backend.models import Document
+from narrant.progress import Progress
 
 
 class PatentConverter:
@@ -37,16 +37,14 @@ class PatentConverter:
             raise ValueError('Country Code {} is unknown'.format(c_code))
         return PatentConverter.COUNTY_PREFIX_REVERS[c_code - 1] + rest
 
-    def convert(self, in_file, out_dir):
+    def convert(self, in_file, out_file):
         """
         `in_file` is the file preprocessed by the Academic library of the TU Braunschweig.
 
         :param in_file: File for the TIB dump
-        :param out_dir: Directory with PubTator files for TIB
+        :param out_file: Output file for the PubTatator file
         :return:
         """
-        if not os.path.exists(out_dir):
-            os.mkdir(out_dir)
         logging.info("Reading file ...")
         title_by_id = dict()
         abstract_by_id = dict()
@@ -65,31 +63,25 @@ class PatentConverter:
                     else:
                         abstract_by_id[did] = body
 
-        sys.stdout.write("Writing {}/{} patents ...".format(int(count / 2), int((idx + 1) / 2)))
         total = len(title_by_id.keys())
-        count = 0
-        last_percentage = 0
-        for did, title in title_by_id.items():
-            if did in abstract_by_id:
-                out_fn = os.path.join(out_dir, "{}.txt".format(did))
-                content = Document.create_pubtator(did, title, abstract_by_id[did])
-                with open(out_fn, "w") as f:
-                    f.write(content + '\n')
-            else:
-                logging.info("WARNING: Document {} has no abstract".format(did))
-            current_percentage = int((count + 1.0) / total * 100.0)
-            if current_percentage > last_percentage:
-                sys.stdout.write("\rWriting {}/{} patents ... {} %".format(count + 1, total,
-                                                                           current_percentage))
-                last_percentage = current_percentage
-            count += 1
-        logging.info(" done")
+        logging.info(f'{total} patents to convert...')
+        progress = Progress(total, print_every=100, text="Converting patents...")
+        progress.start_time()
+        with open(out_file, 'wt') as f_out:
+            for idx, (did, title) in enumerate(title_by_id.items()):
+                progress.print_progress(idx)
+                if did in abstract_by_id:
+                    content = Document.create_pubtator(did, title, abstract_by_id[did])
+                    f_out.write(content + '\n')
+                else:
+                    logging.info("WARNING: Document {} has no abstract".format(did))
+        progress.done()
 
 
 def main():
     parser = ArgumentParser(description="Tool to convert Patent file to Pubtator format")
     parser.add_argument("input", help="Input file", metavar="INPUT_FILE_OR_DIR")
-    parser.add_argument("output", help="Output file/directory", metavar="OUTPUT_FILE_OR_DIR")
+    parser.add_argument("output", help="Output file", metavar="OUTPUT_FILE")
     args = parser.parse_args()
 
     t = PatentConverter()
