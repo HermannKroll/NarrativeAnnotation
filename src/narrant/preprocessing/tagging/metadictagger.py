7a8322c38405de6360b2d17a90cd5aeabf10ab1e
HermannKroll <hermann.kroll@gmail.com>
2021-11-16 17:53:35 +0100

    fix chemical tagging

--- src/narrant/preprocessing/tagging/metadictagger.py
+++ src/narrant/preprocessing/tagging/metadictagger.py
@@ -71,12 +71,11 @@ class MetaDicTagger(dt.DictTagger):
 
 class MetaDicTaggerFactory:
     tagger_by_type: Dict[str, dt.DictTagger] = {
-        et.CHEMICAL: chemical.ChemicalTagger,
         et.DRUG: drug.DrugTagger,
         et.DOSAGE_FORM: dosage.DosageFormTagger,
         et.EXCIPIENT: excipient.ExcipientTagger,
         et.PLANT_FAMILY: plantfamily.PlantFamilyTagger,
-        et.DRUGBANK_CHEMICAL: chemical.ChemicalTagger,
+        et.CHEMBL_CHEMICAL: chemical.ChemicalTagger,
         et.DISEASE: disease.DiseaseTagger,
         et.METHOD: method.MethodTagger,
         et.LAB_METHOD: labmethod.LabMethodTagger
