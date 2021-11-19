4a36c2d9c88df8d13cd402962f0afa82fa37df2d
HermannKroll <hermann.kroll@gmail.com>
2021-11-19 08:34:04 +0100

    simple test script to validate pubtator files

---
+++ src/narrant/pubtator/validate_pubtator.py
@@ -0,0 +1,34 @@
+import argparse
+import logging
+
+from narrant.pubtator.document import TaggedDocument
+
+
+def validate_pubtator_file(input_file:str):
+    from narrant.pubtator.extract import read_pubtator_documents
+    for idx, pubtator_content in enumerate(read_pubtator_documents(input_file)):
+        if not pubtator_content:
+            continue
+        doc = TaggedDocument(pubtator_content)
+
+        if not doc:
+            logging.warning(f'Document could not be parsed from: {pubtator_content}')
+
+        if not doc.title and not doc.abstract:
+            logging.warning(f'Document title and abstract NOT found in: {pubtator_content}')
+
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    args = parser.parse_args(args)
+    logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%Y-%m-%d:%H:%M:%S',
+                        level=logging.INFO)
+
+    validate_pubtator_file(args.input)
+
+
+if __name__ == "__main__":
+    main()
