130145e6e1e277c99603bf56645a487abc4aaf24
HermannKroll <hermann.kroll@gmail.com>
2021-11-22 09:10:57 +0100

    support the creation of partial & complete indexes
    - a partial index must be rebuilt if the db changes
    - a complete index is larger but does not need to be recreated each time

--- src/narrant/build_all_indexes.py
+++ src/narrant/build_all_indexes.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from argparse import ArgumentParser
 
 from narrant.config import BACKEND_CONFIG
 from narrant.entity.entityresolver import MeshResolver, GeneResolver, SpeciesResolver
@@ -8,20 +9,29 @@ from narrant.entity.meshontology import MeSHOntology
 
 
 def main():
+    parser = ArgumentParser(description="Recreates Indexes")
+    parser.add_argument("--skip-mesh", action='store_true', help="Skip the recreation of MeSH Indexes")
+    parser.add_argument("--complete", action='store_true', help="Builds a complete Gene and Species Index...")
+    args = parser.parse_args()
+
     logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                         datefmt='%Y-%m-%d:%H:%M:%S',
                         level=logging.DEBUG)
-    with open(BACKEND_CONFIG) as f:
-        config = json.load(f)
-    print('Some indexes rely on the tag table of the database...')
-    is_sqlite = False or ("use_SQLite" in config and config["use_SQLite"])
-    if is_sqlite:
-        database_name = config["SQLite_path"]
+
+    if not args.complete:
+        with open(BACKEND_CONFIG) as f:
+            config = json.load(f)
+        print('Some indexes rely on the tag table of the database...')
+        is_sqlite = False or ("use_SQLite" in config and config["use_SQLite"])
+        if is_sqlite:
+            database_name = config["SQLite_path"]
+        else:
+            database_name = config["POSTGRES_DB"]
+        print(f'Your current database is: {database_name}')
+        user_input = input('Continue with database setting? (y/yes)')
+        user_input = user_input.lower()
     else:
-        database_name = config["POSTGRES_DB"]
-    print(f'Your current database is: {database_name}')
-    user_input = input('Continue with database setting? (y/yes)')
-    user_input = user_input.lower()
+        user_input = 'y'
     if 'y' in user_input or 'yes' in user_input:
         logging.info('=' * 60)
         logging.info('=' * 60)
@@ -31,26 +41,35 @@ def main():
         logging.info('=' * 60)
         logging.info('=' * 60)
 
-        logging.info('Computing entity ontology index...')
-        entity_ontology = MeSHOntology.instance(load_index=False)
-        entity_ontology.build_index_from_mesh()
-        entity_ontology.store_index()
+        if not args.skip_mesh:
+            logging.info('Computing entity ontology index...')
+            entity_ontology = MeSHOntology.instance(load_index=False)
+            entity_ontology.build_index_from_mesh()
+            entity_ontology.store_index()
+
+            logging.info('Computing MeSH Resolver index...')
+            mesh = MeshResolver()
+            mesh.build_index()
+        else:
+            logging.info('Skipping MeSH Index creation...')
 
         logging.info('Computing Gene Mapping index...')
         gene_mapper = GeneMapper.instance(load_index=False)
         gene_mapper.build_gene_mapper_index()
 
-        logging.info('Computing MeSH Resolver index...')
-        mesh = MeshResolver()
-        mesh.build_index()
-
         logging.info('Computing Gene Resolver index...')
         gene = GeneResolver()
-        gene.build_index()
+        if args.complete:
+            gene.build_index(query_db_gene_ids=False)
+        else:
+            gene.build_index()
 
         logging.info('Computing Species Resolver index...')
         species = SpeciesResolver()
-        species.build_index()
+        if args.complete:
+            species.build_index(query_db_species_ids=False)
+        else:
+            species.build_index()
 
         logging.info('=' * 60)
         logging.info('=' * 60)
