b472fbf84ade1b1c9f218a8081fd1879b71e80d1
HermannKroll <hermann.kroll@gmail.com>
2022-03-23 16:48:42 +0100

    refactor code to methods

--- src/narrant/build_all_indexes.py
+++ src/narrant/build_all_indexes.py
@@ -8,17 +8,11 @@ from narrant.entity.genemapper import GeneMapper
 from narrant.entity.meshontology import MeSHOntology
 
 
-def main():
-    parser = ArgumentParser(description="Recreates Indexes")
-    parser.add_argument("--skip-mesh", action='store_true', help="Skip the recreation of MeSH Indexes")
-    parser.add_argument("--complete", action='store_true', help="Builds a complete Gene and Species Index...")
-    args = parser.parse_args()
-
-    logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-                        datefmt='%Y-%m-%d:%H:%M:%S',
-                        level=logging.DEBUG)
-
-    if not args.complete:
+def build_entity_indexes(complete: bool, skip_mesh: bool, force: bool = False):
+    logging.info('=' * 60)
+    logging.info('=' * 60)
+    logging.info('Building entity translation indexes...')
+    if not complete:
         with open(BACKEND_CONFIG) as f:
             config = json.load(f)
         print('Some indexes rely on the tag table of the database...')
@@ -28,8 +22,11 @@ def main():
         else:
             database_name = config["POSTGRES_DB"]
         print(f'Your current database is: {database_name}')
-        user_input = input('Continue with database setting? (y/yes)')
-        user_input = user_input.lower()
+        if not force:
+            user_input = input('Continue with database setting? (y/yes)')
+            user_input = user_input.lower()
+        else:
+            user_input = 'y'
     else:
         user_input = 'y'
     if 'y' in user_input or 'yes' in user_input:
@@ -41,7 +38,7 @@ def main():
         logging.info('=' * 60)
         logging.info('=' * 60)
 
-        if not args.skip_mesh:
+        if not skip_mesh:
             logging.info('Computing entity ontology index...')
             entity_ontology = MeSHOntology.instance(load_index=False)
             entity_ontology.build_index_from_mesh()
@@ -59,14 +56,14 @@ def main():
 
         logging.info('Computing Gene Resolver index...')
         gene = GeneResolver()
-        if args.complete:
+        if complete:
             gene.build_index(query_db_gene_ids=False)
         else:
             gene.build_index()
 
         logging.info('Computing Species Resolver index...')
         species = SpeciesResolver()
-        if args.complete:
+        if complete:
             species.build_index(query_db_species_ids=False)
         else:
             species.build_index()
@@ -80,6 +77,19 @@ def main():
         logging.info('=' * 60)
     else:
         print('user canceled index creation')
+
+
+def main():
+    parser = ArgumentParser(description="Recreates Indexes")
+    parser.add_argument("--skip-mesh", action='store_true', help="Skip the recreation of MeSH Indexes")
+    parser.add_argument("--complete", action='store_true', help="Builds a complete Gene and Species Index...")
+    args = parser.parse_args()
+
+    logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%Y-%m-%d:%H:%M:%S',
+                        level=logging.DEBUG)
+
+    build_entity_indexes(args.complete, args.skip_mesh)
 
 
 if __name__ == "__main__":
--- src/narrant/build_all_tagging_indexes.py
+++ src/narrant/build_all_tagging_indexes.py
@@ -1,20 +1,32 @@
 import logging
+import os
+import shutil
 
-from narrant.config import PREPROCESS_CONFIG
+from narrant.config import PREPROCESS_CONFIG, TMP_DIR_TAGGER
 from narrant.preprocessing.pharmacy.pharmdicttagger import PharmDictTagger
+
+
+def build_tagging_indexes():
+    logging.info('=='*60)
+    logging.info('Building Tagging Indexes')
+    logging.info('==' * 60)
+    kwargs = dict(logger=logging, config=PREPROCESS_CONFIG, collection="Test")
+    if os.path.exists(TMP_DIR_TAGGER) and os.path.isdir(TMP_DIR_TAGGER):
+        shutil.rmtree(TMP_DIR_TAGGER)
+        os.makedirs(TMP_DIR_TAGGER)
+
+    for ent_type, tagger_class in PharmDictTagger.tagger_by_type.items():
+        logging.info(f'Init tagger for type: {ent_type}')
+        tagger = tagger_class(**kwargs)
+        tagger.prepare()
+    logging.info('==' * 60)
 
 
 def main():
     logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                         datefmt='%Y-%m-%d:%H:%M:%S',
                         level=logging.INFO)
-
-    kwargs = dict(logger=logging, config=PREPROCESS_CONFIG, collection="Test")
-
-    for ent_type, tagger_class in PharmDictTagger.tagger_by_type.items():
-        logging.info(f'Init tagger for type: {ent_type}')
-        tagger = tagger_class(**kwargs)
-        tagger.prepare()
+    build_tagging_indexes()
 
 
 if __name__ == "__main__":
