cb150ab10d49ef352e8fe86e42f18c137320a3d4
HermannKroll <hermann.kroll@gmail.com>
2022-07-21 10:06:22 +0200

    print number of elements in vocabs

--- src/narrant/vocabularies/util/export_pharm_vocabs.py
+++ src/narrant/vocabularies/util/export_pharm_vocabs.py
@@ -23,38 +23,56 @@ def main():
     chemical_term2entities = ChemicalVocabulary.create_chembl_chemical_vocabulary()
     chemical_vocabulary = transform_term2entities_index_to_vocabulary(chemical_term2entities, CHEMICAL)
     chemical_vocabulary.export_vocabulary_as_tsv("pubpharm_chemical_2022.tsv")
+    logging.info(f'Chemical Vocabulary has: {chemical_vocabulary.count_distinct_entities()} unique ids '
+                 f'and {chemical_vocabulary.count_distinct_terms()} unique terms')
 
     drug_term2entities = DrugVocabulary.create_drug_vocabulary_from_chembl(expand_terms=False)
     drug_vocabulary = transform_term2entities_index_to_vocabulary(drug_term2entities, DRUG)
     drug_vocabulary.export_vocabulary_as_tsv("pubpharm_drugs_2022.tsv")
+    logging.info(f'Drug Vocabulary has: {drug_vocabulary.count_distinct_entities()} unique ids '
+                 f'and {drug_vocabulary.count_distinct_terms()} unique terms')
 
     dosage_term2entities = DosageFormVocabulary.create_dosage_form_vocabulary(expand_by_s_and_e=False)
     dosage_vocabulary = transform_term2entities_index_to_vocabulary(dosage_term2entities, DOSAGE_FORM)
     dosage_vocabulary.export_vocabulary_as_tsv("pubpharm_dosage_forms_2022.tsv")
+    logging.info(f'Dosage Form Vocabulary has: {dosage_vocabulary.count_distinct_entities()} unique ids '
+                 f'and {dosage_vocabulary.count_distinct_terms()} unique terms')
 
     disease_term2entities = DiseaseVocabulary.create_disease_vocabulary(expand_by_s_and_e=False)
     disease_vocabulary = transform_term2entities_index_to_vocabulary(disease_term2entities, DISEASE)
     disease_vocabulary.export_vocabulary_as_tsv("pubpharm_diseases_2022.tsv")
+    logging.info(f'Disease Vocabulary has: {disease_vocabulary.count_distinct_entities()} unique ids '
+                 f'and {disease_vocabulary.count_distinct_terms()} unique terms')
 
     excipient_term2entities = ExcipientVocabulary.create_excipient_vocabulary()
     excipient_vocabulary = transform_term2entities_index_to_vocabulary(excipient_term2entities, EXCIPIENT)
     excipient_vocabulary.export_vocabulary_as_tsv("pubpharm_excipients_2022.tsv")
+    logging.info(f'Excipient Vocabulary has: {excipient_vocabulary.count_distinct_entities()} unique ids '
+                 f'and {excipient_vocabulary.count_distinct_terms()} unique terms')
 
     methods_term2entities = MethodVocabulary.create_method_vocabulary(expand_terms=False)
     methods_vocabulary = transform_term2entities_index_to_vocabulary(methods_term2entities, METHOD)
     methods_vocabulary.export_vocabulary_as_tsv("pubpharm_methods_2022.tsv")
+    logging.info(f'Methods Vocabulary has: {methods_vocabulary.count_distinct_entities()} unique ids '
+                 f'and {methods_vocabulary.count_distinct_terms()} unique terms')
 
     labmethods_term2entities = LabMethodVocabulary.create_lab_method_vocabulary(expand_terms=False)
     labmethods_vocabulary = transform_term2entities_index_to_vocabulary(labmethods_term2entities, LAB_METHOD)
     labmethods_vocabulary.export_vocabulary_as_tsv("pubpharm_labmethods_2022.tsv")
+    logging.info(f'Lab Methods Vocabulary has: {labmethods_vocabulary.count_distinct_entities()} unique ids '
+                 f'and {labmethods_vocabulary.count_distinct_terms()} unique terms')
 
     plants_term2entities = PlantFamilyGenusVocabulary.read_plant_family_genus_vocabulary(expand_terms=False)
     plants_vocabulary = transform_term2entities_index_to_vocabulary(plants_term2entities, PLANT_FAMILY_GENUS)
     plants_vocabulary.export_vocabulary_as_tsv("pubpharm_plants_2022.tsv")
+    logging.info(f'Plant Family Vocabulary has: {plants_vocabulary.count_distinct_entities()} unique ids '
+                 f'and {plants_vocabulary.count_distinct_terms()} unique terms')
 
     vaccine_term2entities = VaccineVocabulary.create_vaccine_vocabulary(expand_by_s_and_e=False)
     vaccine_vocabulary = transform_term2entities_index_to_vocabulary(vaccine_term2entities, VACCINE)
     vaccine_vocabulary.export_vocabulary_as_tsv("pubpharm_vaccines_2022.tsv")
+    logging.info(f'Vaccine Vocabulary has: {vaccine_vocabulary.count_distinct_entities()} unique ids '
+                 f'and {vaccine_vocabulary.count_distinct_terms()} unique terms')
 
     pubpharm_vocab_all = Vocabulary("")
     pubpharm_vocab_all.add_vocabulary(chemical_vocabulary)
