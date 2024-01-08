354cc2a41da09cdb3245b246f4c5fccab9b6b2b3
HermannKroll <hermann.kroll@gmail.com>
2024-01-08 14:18:18 +0100

    refactor paths + change crawl date

--- scripts/process_zbmed_for_service.sh
+++ scripts/process_zbmed_for_service.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-ZBMED_JSON="/data/zbmed/zbmed_updates.json"
-# Default : START_CRAWL_DATA="2019-12-01"
-START_CRAWL_DATA="2021-12-01"
-STOP_CRAWL_DATA="2021-12-05"
-ZBMED_PUBTATOR="/data/zbmed/zbmed.json"
-UPDATED_IDS="/data/zbmed/updated_ids.txt"
+DATA_PATH="/data/zbmed/"
+
+ZBMED_JSON="$DATA_PATH"zbmed_updates.json
+ZBMED_PUBTATOR="$DATA_PATH"zbmed.json
+UPDATED_IDS="$DATA_PATH"updated_ids.txt
 
 TAG_CLEANING_SQL=/home/"$USER"/NarrativeAnnotation/sql/clean_tags.sql
 
@@ -13,8 +12,8 @@ TAG_CLEANING_SQL=/home/"$USER"/NarrativeAnnotation/sql/clean_tags.sql
 ## Load everything
 # curl 'https://preview.zbmed.de/api/documents/' -X 'POST' -H 'Content-Type: application/json' -H 'Accept: application/json' --data-binary '{"size":40000,"from":0,"query":{"bool":{"must":[{"range":{"date":{"gte":"2019-12-01||/M","lt":"2021-12-18"}}}]}},"sort":[{"_score":{"order":"asc"}}],"track_total_hits":true}' >  $ZBMED_JSON
 
-# First curl the updates since 01.09.2022
-curl 'https://preview.zbmed.de/api/documents/' -X 'POST' -H 'Content-Type: application/json' -H 'Accept: application/json' --data-binary '{"size":40000,"from":0,"query":{"bool":{"must":[{"range":{"date":{"gte":"2022-09-01||/M"}}}]}},"sort":[{"_score":{"order":"asc"}}],"track_total_hits":true}' > $ZBMED_JSON
+# First curl the updates since 01.12.2023
+curl 'https://preview.zbmed.de/api/documents/' -X 'POST' -H 'Content-Type: application/json' -H 'Accept: application/json' --data-binary '{"size":40000,"from":0,"query":{"bool":{"must":[{"range":{"date":{"gte":"2023-12-01||/M"}}}]}},"sort":[{"_score":{"order":"asc"}}],"track_total_hits":true}' > $ZBMED_JSON
 
 # Load everything
 python3 ~/NarrativeAnnotation/src/narrant/document/load_zbmed_json.py $ZBMED_JSON -c ZBMed
