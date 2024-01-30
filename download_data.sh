4150cfd6e055a8ac0c941531559ada5987979283
HermannKroll <hermann.kroll@gmail.com>
2024-01-30 10:29:43 +0100

    update vocabularies + documentation to do so

--- download_data.sh
+++ download_data.sh
@@ -1,12 +1,12 @@
 mkdir data
 
-wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/desc2023.gz
-gzip -d desc2023.gz
-mv desc2023 data/desc2023.xml
+wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/desc2024.gz
+gzip -d desc2024.gz
+mv desc2024 data/desc.xml
 
-wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/supp2023.gz
-gzip -d supp2023.gz
-mv supp2023 data/supp2023.xml
+wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/supp2024.gz
+gzip -d supp2024.gz
+mv supp2024 data/supp.xml
 
 wget ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz
-mv gene_info.gz data/gene_info_2023.gz
+mv gene_info.gz data/gene_info.gz
