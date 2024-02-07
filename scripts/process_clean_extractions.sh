dc7e0b0edcc526b47718638f9dfb877e4b642ab2
HermannKroll <hermann.kroll@gmail.com>
2024-02-07 11:45:16 +0100

    do not export highest predication id here (do it after index creation in NarrativeIntelligence)

--- scripts/process_clean_extractions.sh
+++ scripts/process_clean_extractions.sh
@@ -26,9 +26,6 @@ if [[ $? != 0 ]]; then
 fi
 
 
-
-
-
 echo "Highest predication id is $PREDICATION_MINIMUM_UPDATE_ID"
 
 # Do the canonicalizing step
@@ -53,9 +50,3 @@ if [[ $? != 0 ]]; then
     exit -1
 fi
 
-# Export highest ID for cleaning
-python3 ~/NarrativeAnnotation/lib/KGExtractionToolbox/src/kgextractiontoolbox/backend/export_highest_predication_id.py $PREDICATION_MINIMUM_UPDATE_ID_FILE
-if [[ $? != 0 ]]; then
-    echo "Previous script returned exit code != 0 -> Stopping pipeline."
-    exit -1
-fi
