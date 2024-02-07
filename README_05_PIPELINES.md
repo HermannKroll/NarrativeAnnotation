c756f20859cf82fd18a6d4b834d8374090122305
HermannKroll <hermann.kroll@gmail.com>
2024-02-07 11:14:13 +0100

    adjust paths in mining scripts + documentation

--- README_05_PIPELINES.md
+++ README_05_PIPELINES.md
@@ -7,6 +7,42 @@ Add the +x flag to the scripts first:
 chmod +x ~/NarrativeAnnotation/scripts/*.sh
 ```
 
+# Adjust the working directory path
+Open all scripts, adjust the paths and adjust the password of the database user:
+- adjust data path
+- adjust SQL paths
+
+# Create env file
+Create an environment file to set DB password as an env variable.
+```
+cd ~/NarrativeAnnotation/scripts
+nano secrets
+```
+
+Paste the following content and replace the password:
+```
+PSQLPW="EXAMPLE_PW"
+```
+
+# Required pipeline data:
+We need two additional files:
+- a biomedical word embedding
+- a trained pharmaceutical technology SVM model
+
+Data is transferred by hand. There is no public download of these models. 
+
+# Setup
+The scripts can process db changes automatically.
+Therefore, we need to know the current starting point (last update).
+Export the latest predication id to a .txt file:
+```
+bash ~/NarrativeAnnotation/scripts/export_highest_predication_id.sh
+```
+This step needs only to be done once. 
+
+
+# Medline Script:
+
 
 The [MEDLINE script](scripts/process_pubmed_updates_for_service.sh) downloads MEDLINE metadata updates from the official NLM API.
 Metadata is then inserted into our database.
@@ -18,6 +54,8 @@ bash ~/NarrativeAnnotation/scripts/process_pubmed_updates_for_service.sh
 ```
 
 
+# ZB MED Script
+
 We implemented a similar [script for the ZB MED pre-prints](scripts/process_zbmed_for_service.sh). 
 New pre-prints are crawled, transformed, loaded, tagged, classified and statements are extracted from them.
 
@@ -25,6 +63,8 @@ New pre-prints are crawled, transformed, loaded, tagged, classified and statemen
 ```
 bash ~/NarrativeAnnotation/scripts/process_zbmed_for_service.sh
 ```
+
+# Clean Data
 
 
 Extractions are not cleaned by the previous scripts. 
--- scripts/process_clean_extractions.sh
+++ scripts/process_clean_extractions.sh
@@ -1,11 +1,38 @@
-PREDICATION_CLEANING_SQL=/home/"$USER"/NarrativeAnnotation/sql/clean_predication.sql
-PREDICATION_MINIMUM_UPDATE_ID_FILE=/home/"$USER"/db_highest_predication_id.txt
+# get the highest predication id
+PREDICATION_MINIMUM_UPDATE_ID_FILE="~/NarrativeAnnotation/scripts/highest_predication_id.txt"
 PREDICATION_MINIMUM_UPDATE_ID=$(<"$PREDICATION_MINIMUM_UPDATE_ID_FILE")
+if [[ $? != 0 ]]; then
+    echo "Previous script returned exit code != 0 -> Stopping pipeline."
+    exit -1
+fi
+
+
+if [ "$(id -u)" == 0 ]; then
+  PREDICATION_CLEANING_SQL=/home/NarrativeAnnotation/sql/clean_predication.sql
+  echo "root"
+fi
+if [ "$(id -u)" -ne 0 ]; then
+  PREDICATION_CLEANING_SQL=/home/"$USER"/NarrativeAnnotation/sql/clean_predication.sql
+  echo "not root"
+fi
+
+
+
+# load the db password
+source .secret
+if [[ $? != 0 ]]; then
+    echo "Previous script returned exit code != 0 -> Stopping pipeline."
+    exit -1
+fi
+
+
+
+
 
 echo "Highest predication id is $PREDICATION_MINIMUM_UPDATE_ID"
 
 # Do the canonicalizing step
-python3 ~/NarrativeAnnotation/lib/KGExtractionToolbox/src/kgextractiontoolbox/cleaning/canonicalize_predicates.py --word2vec_model /data/workingdir/BioWordVec_PubMed_MIMICIII_d200.bin --relation_vocab ~/NarrativeAnnotation/resources/pharm_relation_vocab.json --predicate_id_minimum $PREDICATION_MINIMUM_UPDATE_ID
+python3 ~/NarrativeAnnotation/lib/KGExtractionToolbox/src/kgextractiontoolbox/cleaning/canonicalize_predicates.py --word2vec_model /data/FID_Pharmazie_Services/narrative_data_update/BioWordVec_PubMed_MIMICIII_d200.bin --relation_vocab ~/NarrativeAnnotation/resources/pharm_relation_vocab.json --predicate_id_minimum $PREDICATION_MINIMUM_UPDATE_ID
 if [[ $? != 0 ]]; then
     echo "Previous script returned exit code != 0 -> Stopping pipeline."
     exit -1
@@ -20,7 +47,7 @@ fi
 
 # Execute Cleaning Rules for Predications
 echo 'cleaning predication table with hand-written rules'
-psql "host=127.0.0.1 port=5432 dbname=fidpharmazie user=mininguser password=F2M>FAJL2ptVm)W" -f $PREDICATION_CLEANING_SQL
+psql "host=127.0.0.1 port=5432 dbname=fidpharmazie user=mininguser password=PSQLPW" -f $PREDICATION_CLEANING_SQL
 if [[ $? != 0 ]]; then
     echo "Previous script returned exit code != 0 -> Stopping pipeline."
     exit -1
