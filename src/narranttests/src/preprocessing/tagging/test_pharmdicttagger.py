eea2791645968dd9ac166be905eae04fbe9c74fa
Pascal Sackhoff <p.sackhoff@tu-bs.de>
2024-03-14 17:21:01 +0100

    Fixed pharmdicttagger instantiation; added new testcase for excluded types of tags with having shorter lengths than config.len

--- src/narranttests/src/preprocessing/tagging/test_pharmdicttagger.py
+++ src/narranttests/src/preprocessing/tagging/test_pharmdicttagger.py
@@ -3,7 +3,7 @@ import unittest
 
 import narrant.entitylinking.enttypes as et
 import narranttests.util as util
-from kgextractiontoolbox.document.document import parse_tag_list, TaggedEntity
+from kgextractiontoolbox.document.document import parse_tag_list, TaggedEntity, TaggedDocument
 from kgextractiontoolbox.document.extract import read_tagged_documents
 from narrant.entitylinking.pharmacy.pharmdicttagger import PharmDictTagger
 
@@ -12,7 +12,7 @@ class TestMetadictagger(unittest.TestCase):
     ent_type_set = {et.DRUG, et.EXCIPIENT, et.DOSAGE_FORM, et.PLANT_FAMILY_GENUS}
 
     def test_init(self):
-        metatag = TestMetadictagger.make_metatag()
+        metatag = TestMetadictagger.make_metatag(self.ent_type_set)
         self.assertSetEqual(set(metatag._vocabs.keys()), TestMetadictagger.ent_type_set)
         self.assertIn("MESH:D007267", metatag._vocabs[et.DOSAGE_FORM]["injection"])
         self.assertIn("Agave", metatag._vocabs[et.PLANT_FAMILY_GENUS]["agave"])
@@ -24,7 +24,7 @@ class TestMetadictagger(unittest.TestCase):
         os.makedirs(os.path.dirname(out_1), exist_ok=True)
         in_2 = util.get_test_resource_filepath("infiles/test_metadictagger/5600.txt")
         out_2 = util.tmp_rel_path("out/5600.txt")
-        metatag = TestMetadictagger.make_metatag()
+        metatag = TestMetadictagger.make_metatag(self.ent_type_set)
         metatag._tag(in_1, out_1)
         metatag._tag(in_2, out_2)
         tags_1 = [repr(tag) for tag in parse_tag_list(out_1)]
@@ -34,7 +34,7 @@ class TestMetadictagger(unittest.TestCase):
 
     def test_custom_abbreviation(self):
         in_file = util.get_test_resource_filepath("infiles/test_metadictagger/abbreviations.txt")
-        metatag = TestMetadictagger.make_metatag()
+        metatag = TestMetadictagger.make_metatag(self.ent_type_set)
         out_file = metatag.tag_doc([d for d in read_tagged_documents(in_file)][0])
         out_file.clean_tags()
         self.assertIn(TaggedEntity(None, 32926486, 716, 718, "eo", "Excipient", "CHEMBL1743219"), out_file.tags)
@@ -42,7 +42,7 @@ class TestMetadictagger(unittest.TestCase):
 
     def test_custom_abbreviation_with_closing_space(self):
         in_file = util.get_test_resource_filepath("infiles/test_metadictagger/h2o2space_test.txt")
-        metatag = TestMetadictagger.make_metatag()
+        metatag = TestMetadictagger.make_metatag(self.ent_type_set)
         out_file = metatag.tag_doc([d for d in read_tagged_documents(in_file)][0])
         out_file.clean_tags()
         self.assertIn(TaggedEntity(None, 32926513, 61, 78, "hydrogen peroxide", "Excipient", "CHEMBL71595"),
@@ -58,13 +58,31 @@ class TestMetadictagger(unittest.TestCase):
         self.assertIn(TaggedEntity(None, 32926513, 949, 956, "h 2 o 2", "Excipient", "CHEMBL71595"), out_file.tags)
         self.assertIn(TaggedEntity(None, 32926513, 1172, 1179, "h 2 o 2", "Excipient", "CHEMBL71595"), out_file.tags)
 
+    def test_different_min_full_tag_lengths(self):
+        ent_type_set = {et.DRUG, et.HEALTH_STATUS}
+        tagger = TestMetadictagger.make_metatag(ent_type_set)
+
+        doc_text = "Man should not be tagged, men should. The drug tagger should tag metformin but not drug."
+        doc = TaggedDocument(title=doc_text, abstract="", id=1)
+        doc = tagger.tag_doc(doc)
+        doc.sort_tags()
+        self.assertEqual(2, len(doc.tags))
+
+        # <Entity 26,29,men,HealthStatus,MESH:D008571>
+        actual_tag = doc.tags[0]
+        expected_tag = TaggedEntity(None, 1, 26, 29, "men", et.HEALTH_STATUS, "MESH:D008571")
+        self.assertEqual(expected_tag, actual_tag)
+
+        # <Entity 65,74,metformin,Drug,CHEMBL1431>
+        actual_tag = doc.tags[1]
+        expected_tag = TaggedEntity(None, 1, 65, 74, "metformin", et.DRUG, "CHEMBL1431")
+        self.assertEqual(expected_tag, actual_tag)
+
     @staticmethod
-    def make_metatag():
-        factory = PharmDictTagger(TestMetadictagger.ent_type_set,
-                                  util.create_test_kwargs())
-        metatag = factory.create_MetaDicTagger()
-        metatag.prepare()
-        return metatag
+    def make_metatag(ent_type_set) -> PharmDictTagger:
+        tagger = PharmDictTagger(ent_type_set, util.create_test_kwargs())
+        tagger.prepare()
+        return tagger
 
 
 def assert_tags_pmc_4297_5600(test_suit, tags_4297, tags_5600):
