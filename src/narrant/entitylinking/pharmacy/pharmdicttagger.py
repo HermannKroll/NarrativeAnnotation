f580e44a793da7d1323621b54ab69470101b796a
Pascal Sackhoff <p.sackhoff@tu-bs.de>
2024-03-14 17:16:46 +0100

    Pharmdictagger is now child of metadictagger; added function to exclude tag-types from cleaning procedure

--- src/narrant/entitylinking/pharmacy/pharmdicttagger.py
+++ src/narrant/entitylinking/pharmacy/pharmdicttagger.py
@@ -1,6 +1,8 @@
 import logging
-from typing import Dict
+from typing import Dict, List
 
+from kgextractiontoolbox.document.document import TaggedEntity
+from kgextractiontoolbox.entitylinking.tagging.dictagger import DictTagger
 from kgextractiontoolbox.entitylinking.tagging.metadictagger import MetaDicTagger
 from narrant.entitylinking import enttypes as et
 from narrant.entitylinking.pharmacy import chemical, drug, method, labmethod, excipient, plantfamilygenus, disease, \
@@ -8,7 +10,7 @@ from narrant.entitylinking.pharmacy import chemical, drug, method, labmethod, ex
 from narrant.entitylinking.tagging import indexed_dictagger as dt
 
 
-class PharmDictTagger:
+class PharmDictTagger(MetaDicTagger):
     __name__ = "PharmDictTagger"
     __version__ = "1.0"
 
@@ -27,24 +29,43 @@ class PharmDictTagger:
         et.TISSUE: tissue.TissueTagger
     }
 
+    ignored_tag_types_to_clean = {
+        et.HEALTH_STATUS
+    }
+
     @staticmethod
     def get_supported_tagtypes():
         return set(PharmDictTagger.tagger_by_type.keys())
 
     def __init__(self, tag_types, tagger_kwargs):
+        super().__init__(**tagger_kwargs)
         self.tag_types = tag_types
-        self.tagger_kwargs = tagger_kwargs
 
-    def create_MetaDicTagger(self):
-        metatag = MetaDicTagger(**self.tagger_kwargs)
         for tag_type in self.tag_types:
             subtagger = PharmDictTagger.tagger_by_type.get(tag_type)
             if not subtagger:
                 logging.warning(f"No tagging class found for tagtype {tag_type}!")
                 continue
-            metatag.add_tagger(subtagger(**self.tagger_kwargs))
+            super().add_tagger(subtagger(**tagger_kwargs))
 
-        # reset taggers name and version
-        metatag.__name__ = self.__name__
-        metatag.__version__ = self.__version__
-        return metatag
+        self.clean_abbreviation_tags_function = self.clean_abbreviation_tags_pharmacy
+
+    def clean_abbreviation_tags_pharmacy(self, tags: List[TaggedEntity], minimum_tag_len: int):
+        """
+        This method sorts the tags by type and cleans up those that should have not an ignored type. This retains
+        short but important tags, e.g. "men" with entity_type=HEALTH_STATUS.
+        :param minimum_tag_len: the minimum tag length to treat a term as a 'full' tag
+        :param tags: a list of tags
+        :return: a list of cleaned tags
+        """
+        tags_ignored = list()
+        tags_to_clean = list()
+        for tag in tags:
+            if tag.ent_type in self.ignored_tag_types_to_clean:
+                tags_ignored.append(tag)
+            else:
+                tags_to_clean.append(tag)
+
+        tags = DictTagger.clean_abbreviation_tags(tags_to_clean, minimum_tag_len)
+        tags += tags_ignored
+        return tags
