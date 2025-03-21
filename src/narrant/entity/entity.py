15d1463768aed850cfb422c6dace462a42def50c
HermannKroll <hermann.kroll@gmail.com>
2025-03-21 16:59:21 +0100

    #318 remove not used code

--- src/narrant/entity/entity.py
+++ src/narrant/entity/entity.py
@@ -1,6 +1,3 @@
-from narrant.entity.meshontology import MeSHOntology
-
-
 class Entity:
 
     def __init__(self, entity_id, entity_type, entity_name=None, entity_class=None):
@@ -20,18 +17,6 @@ class Entity:
 
     def __repr__(self):
         return '{} ({})'.format(self.entity_id, self.entity_type)
-
-    def get_meshs(self) -> {str}:
-        """
-        Lookup all (sub-)mesh-descriptors, if type is 'MESH_ONTOLOGY'. Defaults to entity_id
-        :return: {str}
-        """
-        mesh_ontology = MeSHOntology()
-        if self.entity_type == 'MESH_ONTOLOGY':
-            mesh_descs = set(mesh_ontology.find_descriptors_start_with_tree_no(self.entity_id))
-            return map(lambda d: 'MESH:{}'.format(d[0]), mesh_descs)
-        else:
-            return {self.entity_id}
 
     def to_dict(self):
         return dict(entity_id=self.entity_id, entity_type=self.entity_type, entity_name=self.entity_name)
