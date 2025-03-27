1518490e1581ebcb8e131dcc8c74967df9a13f6d
HermannKroll <hermann.kroll@gmail.com>
2025-03-27 16:26:50 +0100

    #313 refactor entity key to this entity.py

--- src/narrant/entity/entity.py
+++ src/narrant/entity/entity.py
@@ -1,3 +1,13 @@
+def get_unique_entity_key(entity_type: str, entity_id: str) -> str:
+    """
+    Generates a unique entity key as a string
+    :param entity_type: entity type
+    :param entity_id: entity id
+    :return: unique entity key
+    """
+    return '___'.join([entity_type, entity_id])
+
+
 class Entity:
 
     def __init__(self, entity_id, entity_type, entity_name=None, entity_class=None):
@@ -7,7 +17,7 @@ class Entity:
         self.entity_class = entity_class
 
     def __hash__(self):
-        return f'{self.entity_type}||{self.entity_id}'.__hash__()
+        return hash(self.get_unique_key())
 
     def __eq__(self, other):
         return other.entity_id == self.entity_id and other.entity_type == self.entity_type
@@ -20,3 +30,6 @@ class Entity:
 
     def to_dict(self):
         return dict(entity_id=self.entity_id, entity_type=self.entity_type, entity_name=self.entity_name)
+
+    def get_unique_key(self):
+        return get_unique_entity_key(entity_type=self.entity_type, entity_id=self.entity_id)
