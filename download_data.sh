66d358789f65da014999611017ba35d2602234d3
HermannKroll <hermann.kroll@gmail.com>
2025-04-09 09:44:04 +0200

    update mesh version

--- download_data.sh
+++ download_data.sh
@@ -1,12 +1,12 @@
 mkdir data
 
-wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/desc2024.gz
-gzip -d desc2024.gz
-mv desc2024 data/desc.xml
+wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/desc2025.gz
+gzip -d desc2025.gz
+mv desc2025 data/desc.xml
 
-wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/supp2024.gz
-gzip -d supp2024.gz
-mv supp2024 data/supp.xml
+wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/supp2025.gz
+gzip -d supp2025.gz
+mv supp2025 data/supp.xml
 
 wget ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz
 mv gene_info.gz data/gene_info.gz
