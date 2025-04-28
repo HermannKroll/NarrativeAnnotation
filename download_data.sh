0d126b2c33d82260abc70ed9684323d8e7c3cf94
Pascal Sackhoff <p.sackhoff@tu-bs.de>
2025-04-28 12:12:23 +0200

    #319 adjust download_data.sh to be compatible with windows

--- download_data.sh
+++ download_data.sh
@@ -1,15 +1,23 @@
 mkdir data
 
-wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/desc2025.gz
+if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* || "$OSTYPE" == "freebsd" ]]; then
+  wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/desc2025.gz
+  wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/supp2025.gz
+  wget ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz
+  wget https://ftp.expasy.org/databases/cellosaurus/cellosaurus.xml
+elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+  curl ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/desc2025.gz -o desc2025.gz
+  curl ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/supp2025.gz -o supp2025.gz
+  curl ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz -o gene_info.gz
+  curl https://ftp.expasy.org/databases/cellosaurus/cellosaurus.xml -o cellosaurus.xml
+fi
+
 gzip -d desc2025.gz
 mv desc2025 data/desc.xml
 
-wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/supp2025.gz
 gzip -d supp2025.gz
 mv supp2025 data/supp.xml
 
-wget ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz
 mv gene_info.gz data/gene_info.gz
 
-wget https://ftp.expasy.org/databases/cellosaurus/cellosaurus.xml
 mv cellosaurus.xml data/cellosaurus.xml
