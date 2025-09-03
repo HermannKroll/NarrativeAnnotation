6955b737233832860d81576c1124a2f1c2cd2e54
HermannKroll <hermann.kroll@gmail.com>
2025-09-03 15:44:00 +0200

    improve subquery generation

--- src/narrant/cleaning/pharmaceutical_rules.py
+++ src/narrant/cleaning/pharmaceutical_rules.py
@@ -176,7 +176,7 @@ def check_type_constraints(reorder_tuples=True, document_collection: str = None,
 
     logging.info(f'Mapping {len(preds_to_associate)} predications to associate...')
     insert_predication_ids_to_delete(preds_to_associate)
-    subquery = session.query(PredicationToDelete.predication_id).subquery()
+    subquery = session.query(PredicationToDelete.predication_id)
     stmt = update(Predication).where(Predication.id.in_(subquery)).values(relation=ASSOCIATED_PREDICATE_UNSURE)
     session.execute(stmt, execution_options=immutabledict({"synchronize_session": 'fetch'}))
     logging.debug('Committing...')
@@ -213,7 +213,7 @@ def check_type_constraints(reorder_tuples=True, document_collection: str = None,
         logging.info(f'Insert {len(predication_values)} reordered predications to database')
         Predication.bulk_insert_values_into_table(session, predication_values, check_constraints=False)
         logging.info(f'Deleting {len(preds_to_reorder)} old and wrongly ordered predications')
-        subquery = session.query(PredicationToDelete.predication_id).subquery()
+        subquery = session.query(PredicationToDelete.predication_id)
         stmt = delete(Predication).where(Predication.id.in_(subquery))
         session.execute(stmt, execution_options=immutabledict({"synchronize_session": 'fetch'}))
         logging.debug('Committing...')
